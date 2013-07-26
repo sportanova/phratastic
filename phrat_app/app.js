a5651d0cd2e793457a87552b1f8c9d8d1c0f7250
Stephen Portanova <portanova@Stevo.local>
2013-07-25 20:09:54 -0700

    added registration

--- phrat_app/app.js
+++ phrat_app/app.js
@@ -1,8 +1,10 @@
-var express = require('express'),
-  routes = require('./routes'),
-  user = require('./routes/user'),
-  http = require('http'),
-  path = require('path');
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path'),
+  base64url = require('b64url'),
+  crypto = require('crypto');
 
 var app = express();
 
@@ -24,6 +26,42 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+app.get('/register', function(req, res){
+  res.render('register', { title: 'Express' });
+});
+
+app.post('/hey', function(req, res){
+  var part1, part2, signedRequest;
+  signedRequest = req.body.signed_request.split('.');
+  part1 = signedRequest[0];
+  part2 = signedRequest[1];
+  // var decodedPart1 = b64url.decode(part1);
+  // console.log(decodedPart1.toString());
+
+  function parse_signed_request(signed_request, secret) {
+    encoded_data = signed_request.split('.',2);
+    // decode the data
+    sig = encoded_data[0];
+    json = base64url.decode(encoded_data[1]);
+    data = JSON.parse(json); // ERROR Occurs Here!
+    // check algorithm - not relevant to error
+    if (!data.algorithm || data.algorithm.toUpperCase() != 'HMAC-SHA256') {
+        console.error('Unknown algorithm. Expected HMAC-SHA256');
+        return null;
+    }
+    // check sig - not relevant to error
+    expected_sig = crypto.createHmac('sha256',secret).update(encoded_data[1]).digest('base64').replace(/\+/g,'-').replace(/\//g,'_').replace('=','');
+    if (sig !== expected_sig) {
+        console.error('Bad signed JSON Signature!');
+        return null;
+    }
+    return data;
+  }
+  var a = parse_signed_request(req.body.signed_request, 'b5c82944f3f4dee207900526d32fa45c');
+  console.log('signed request', a);
+  res.render('just_registered', { title: 'Express' });
+});
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
