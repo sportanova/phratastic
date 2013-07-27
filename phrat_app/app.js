2df8ec3aeff9157f29e4727cce29cfb2c5e4dd7b
Stephen Portanova <portanova@Stevo.local>
2013-07-27 13:45:53 -0700

    inserts into db, but doesn't redirect properly

--- phrat_app/app.js
+++ phrat_app/app.js
@@ -6,6 +6,10 @@ var express = require('express')
   , base64url = require('b64url')
   , crypto = require('crypto')
   , app = express()
+  , passport = require('passport')
+  , FacebookStrategy = require('passport-facebook').Strategy
+  , Sequelize = require('sequelize')
+  , sequelize = new Sequelize('test', 'root');
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -25,57 +29,64 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/users', user.list);
-app.get('/register', function(req, res){
-  res.render('register', { title: 'Express' });
+
+app.get('/channel', function(req, res){
+  res.render('channel', { title: 'Express' });
 });
 
 app.get('/login', function(req, res){
   res.render('login', { title: 'Express' });
 });
 
-app.get('/channel', function(req, res){
-  res.render('channel', { title: 'Express' });
+app.get('/home', function(req, res){
+  res.render('home', { title: 'Express' });
 });
 
-app.post('/hey', function(req, res){
-  function parse_signed_request(signed_request, secret) {
-    encoded_data = signed_request.split('.',2);
-    // decode the data
-    sig = encoded_data[0];
-    json = base64url.decode(encoded_data[1]);
-    data = JSON.parse(json); // ERROR Occurs Here!
-    // check algorithm - not relevant to error
-    if (!data.algorithm || data.algorithm.toUpperCase() != 'HMAC-SHA256') {
-        console.error('Unknown algorithm. Expected HMAC-SHA256');
-        return null;
-    }
-    // check sig - not relevant to error
-    expected_sig = crypto.createHmac('sha256',secret).update(encoded_data[1]).digest('base64').replace(/\+/g,'-').replace(/\//g,'_').replace('=','');
-    if (sig !== expected_sig) {
-        console.error('Bad signed JSON Signature!');
-        return null;
-    }
-    return data;
-  }
-  var fbInfo = parse_signed_request(req.body.signed_request, 'b5c82944f3f4dee207900526d32fa45c');
-  console.log('signed request', fbInfo);
-  res.render('just_registered', { title: 'Express' });
+passport.use(new FacebookStrategy({
+    clientID: '696227333737725',
+    clientSecret: 'b5c82944f3f4dee207900526d32fa45c',
+    callbackURL: "http://127.0.0.1:3000/auth/facebook/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    // User.findOrCreate(..., function(err, user) {
+    //   if (err) { return done(err); }
+    //   done(null, user);
+    // });
+    console.log('anonymous function called');
+    var User = sequelize.define('User', {
+        username: Sequelize.STRING,
+        birthday: Sequelize.STRING,
+        message: Sequelize.STRING
+      });
 
-  var Sequelize = require('sequelize');
-  var sequelize = new Sequelize('test', 'root');
-
-  var User = sequelize.define('User', {
-    username: Sequelize.STRING,
-    birthday: Sequelize.STRING,
-    message: Sequelize.STRING
-  });
-
-  User.sync().success(function() {
-    var newUser = User.build({username: fbInfo.registration.name, birthday: fbInfo.registration.birthday});
-    newUser.save().success(function() {
+    User.sync().success(function() {
+      var newUser = User.build({username: 'esteban', birthday: 'old'});
+      newUser.save().success(function() {
+      });
     });
-  });
-});
+  }
+));
+
+app.get('/auth/facebook', passport.authenticate('facebook'));
+
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', {
+    successRedirect: '/',
+    failureRedirect: '/login'
+  })
+);
+
+  // var User = sequelize.define('User', {
+  //   username: Sequelize.STRING,
+  //   birthday: Sequelize.STRING,
+  //   message: Sequelize.STRING
+  // });
+
+  // User.sync().success(function() {
+  //   var newUser = User.build({username: fbInfo.registration.name, birthday: fbInfo.registration.birthday});
+  //   newUser.save().success(function() {
+  //   });
+  // });
 
 
 http.createServer(app).listen(app.get('port'), function(){
