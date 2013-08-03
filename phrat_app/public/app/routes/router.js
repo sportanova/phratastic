84ceb1b7c41c2062cfdc1b9dec5bf23d95f25dd5
Sportanova <sportano@gmail.com>
2013-08-02 21:40:14 -0700

    rendering within router

--- phrat_app/public/app/routes/router.js
+++ phrat_app/public/app/routes/router.js
@@ -6,8 +6,12 @@ var Router = Backbone.Router.extend({
 
   initialize: function() {
   },
+  // http://127.0.0.1:3000/back#test
   test: function(){
     console.log('hey');
+    window.app = new App();
+      var appView = new AppView({model: app});
+      $('#main').append(appView.render());
   }
 
 });
