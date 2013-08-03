c34f5484c2e9bb757fcf83790cee1387d510338b
Sportanova <sportano@gmail.com>
2013-08-02 21:43:39 -0700

    changed route name

--- phrat_app/public/app/routes/router.js
+++ phrat_app/public/app/routes/router.js
@@ -1,17 +1,17 @@
 var Router = Backbone.Router.extend({
 
   routes: {
-    'test': 'test'
+    'recruits': 'recruits'
   },
 
   initialize: function() {
   },
   // http://127.0.0.1:3000/back#test
-  test: function(){
+  recruits: function(){
     console.log('hey');
     window.app = new App();
-      var appView = new AppView({model: app});
-      $('#main').append(appView.render());
+    var appView = new AppView({model: app});
+    $('#main').append(appView.render());
   }
 
 });
