e41c0b55fff8a516751ae6437594860d71757226
Sportanova <sportano@gmail.com>
2013-08-05 10:49:21 -0700

    added register route

--- phrat_app/public/app/routes/router.js
+++ phrat_app/public/app/routes/router.js
@@ -1,17 +1,21 @@
 var Router = Backbone.Router.extend({
 
   routes: {
-    'recruits': 'recruits'
+    'recruits': 'recruits',
+    'register': 'register'
   },
 
   initialize: function() {
   },
   // http://127.0.0.1:3000/back#test
   recruits: function(){
-    console.log('hey');
     window.app = new App();
     var appView = new AppView({model: app});
     $('#main').append(appView.render());
+  },
+
+  register: function(){
+    console.log('register');
   }
 
 });
