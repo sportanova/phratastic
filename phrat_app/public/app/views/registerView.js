47a187d58a4a9dcc9f2a87cb9ddb980fc35a3fde
Sportanova <sportano@gmail.com>
2013-08-09 12:06:51 -0700

    styled recruitHome view

--- phrat_app/public/app/views/registerView.js
+++ phrat_app/public/app/views/registerView.js
@@ -2,15 +2,22 @@ var RegisterView = Backbone.View.extend({
   initialize: function() {
   },
   render: function(){
-    return this.$el.html(
-      '<div> \
-        If you are already a fraternity member, enter your member \
-        key here \
+    return this.$el.html(' \
+      <div class="memberKeyInfo"> \
+        <div class="memberKey"> \
+          If you\'re already in a fraternity, enter your member \
+          key here \
+        </div> \
+        <form class="memberKeyForm" action="/memberConfirm" method="POST"> \
+          <input class="memberKeyInput" type="text" name="confirm"/> \
+          <input type="submit" value="submit" /> \
+        </form> \
       </div> \
-      <form action="/memberConfirm" method="POST"> \
-        <input type="text" name="confirm"/> \
-        <input type="submit" value="submit" /> \
-      </form> \
+      <div class="recruitInstructions"> \
+        <p class="recruitWatch"> If you\'re a recruit, get pumped for the fraternity experience </br> \
+        by watching some college party movie scenes! </p>\
+      <iframe class="partyYouTube" width="560" height="315" src="//www.youtube.com/embed/fdgYRdBdJl0?rel=0" frameborder="0" allowfullscreen></iframe> \
+      </div> \
       ');
   }
 });
