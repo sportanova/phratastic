0e373b398fb3f6f5b21d8376eed29d5121faf940
Sportanova <sportano@gmail.com>
2013-08-12 16:13:52 -0700

    added video to logout page

--- phrat_app/public/app/views/logoutView.js
+++ phrat_app/public/app/views/logoutView.js
@@ -1,7 +1,11 @@
 var LogoutView = Backbone.View.extend({
   render: function() {
     return this.$el.html(' \
-      <div> You Logged out </div> \
+      <div class="loggedOut"> You logged out. </div> \
+      <div class="loggedOut2"> Now we feel like crying... </div> \
+      <div class="timGreen"> \
+        <iframe width="560" height="315" src="//www.youtube.com/embed/B8C-q_PCiVI?rel=0" frameborder="0" allowfullscreen></iframe> \
+      </div> \
     ');
   }
 });
