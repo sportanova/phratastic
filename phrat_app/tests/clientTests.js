12aaef722d162d97f8655b129a9e3b17279e6555
Sportanova <sportano@gmail.com>
2013-08-12 10:14:43 -0700

    added test to check instantiation of views

--- phrat_app/tests/clientTests.js
+++ phrat_app/tests/clientTests.js
@@ -9,4 +9,21 @@ describe('instantiation', function() {
     expect(recruitModel).to.be.an.instanceof(RecruitModel);
     expect(registerModel).to.be.an.instanceof(RegisterModel);
   });
+
+  it('should instantiate views', function() {
+    var app = new App();
+
+    var appView = new AppView({model: app});
+    var recruitsListView = new RecruitsListView();
+    var recruitView = new RecruitView();
+    var registerView = new RegisterView();
+    var headerView =  new HeaderView();
+    var homeView =  new HomeView();
+    expect(appView).to.be.an.instanceof(AppView);
+    expect(recruitsListView).to.be.an.instanceof(RecruitsListView);
+    expect(recruitView).to.be.an.instanceof(RecruitView);
+    expect(registerView).to.be.an.instanceof(RegisterView);
+    expect(headerView).to.be.an.instanceof(HeaderView);
+    expect(homeView).to.be.an.instanceof(HomeView);
+  });
 });
